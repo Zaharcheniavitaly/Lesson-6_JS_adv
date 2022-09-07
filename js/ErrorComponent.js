343a718e0194a0e7617ff03e4338ad475cce3a06
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-09-07 19:14:19 +0300

    Update ErrorComponent.js

--- js/ErrorComponent.js
+++ js/ErrorComponent.js
@@ -3,7 +3,7 @@
 Vue.component('error-comp', {
 	template: `	
 		<div class="container" v-show="$root.error">
-			<p class="error" v-if="!error" >Не правильно указан путь к товару!!!!</p>
+			<p class="error" v-if="!error" >Не правильно указан путь к товару!!!</p>
 		</div>
 	`
 });
